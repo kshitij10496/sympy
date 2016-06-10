5fa3f8087dd033b2a83e940d0967fc09da8e9087
kshitij10496 <KshitijSaraogi@gmail.com>
2016-06-10 18:58:01 +0530

    added tests for function_range and continuous_in

--- sympy/calculus/tests/test_singularities.py
+++ sympy/calculus/tests/test_singularities.py
@@ -1,9 +1,9 @@
-from sympy import Symbol, exp, log
-from sympy.calculus.singularities import (singularities, is_increasing,
+from sympy.calculus.singularities import (singularities, function_range,
+                                          continuous_in, is_increasing,
                                           is_strictly_increasing, is_decreasing,
                                           is_strictly_decreasing, is_monotonic)
-from sympy.sets import Interval, FiniteSet, EmptySet
-from sympy import oo, S, I, sqrt
+from sympy.sets import Interval, FiniteSet, EmptySet, Union
+from sympy import Symbol, exp, log,  oo, S, I, sqrt, sin, tan, pi
 
 from sympy.utilities.pytest import XFAIL
 
@@ -23,12 +23,39 @@ def test_singularities():
                                                        (1 + sqrt(3)*I)/2)
     assert singularities(1/(y**2 + 2*I*y + 1), y) == FiniteSet(-I + sqrt(2)*I, -I - sqrt(2)*I)
 
+
 @XFAIL
 def test_singularities_non_rational():
     x = Symbol('x', real=True)
 
     assert singularities(exp(1/x), x) == (0)
     assert singularities(log((x - 2)**2), x) == (2)
+
+
+def test_function_range():
+    x = Symbol('x')
+    assert function_range(sin(x), x, Interval(-pi/2, pi/2)) == Interval(-1, 1)
+    assert function_range(sin(x), x, Interval(0, pi)) == Interval(0, 1)
+    assert function_range(tan(x), x, Interval(0, pi)) == Interval(-oo, oo)
+    assert function_range(tan(x), x, Interval(pi/2, pi)) == Interval(-oo, 0)
+    assert function_range((x + 3)/(x - 2), x, Interval(-5, 5)) == Interval(-oo, oo)
+    assert function_range(1/(x**2), x, Interval(-1, 1)) == Interval(1, oo)
+    assert function_range(exp(x), x, Interval(-1, 1)) == Interval(exp(-1), exp(1))
+    assert function_range(log(x) - x, x, S.Reals) == Interval(-oo, -1)
+    assert function_range(sqrt(3*x - 1), x, Interval(0, 2)) == Interval(0, sqrt(5))
+
+
+def test_continuous_in():
+    x = Symbol('x')
+    assert continuous_in(sin(x), x, Interval(0, 2*pi)) == Interval(0, 2*pi)
+    assert continuous_in(tan(x), x, Interval(0, 2*pi)) == \
+        Union(Interval(0, pi/2, False, True), Interval(pi/2, 3*pi/2, True, True),
+              Interval(3*pi/2, 2*pi, True, False))
+    assert continuous_in((x - 1)/((x - 1)**2), x, S.Reals) == \
+        Union(Interval(-oo, 1, True, True), Interval(1, oo, True, True))
+    assert continuous_in(log(x) + log(4*x - 1), x, S.Reals) == \
+        Interval(1/4, oo, True, True)
+    assert continuous_in(1/sqrt(x - 3), x, S.Reals) == Interval(3, oo, True, True)
 
 
 def test_is_increasing():
